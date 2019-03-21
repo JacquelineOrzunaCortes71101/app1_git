017214579115e8a525ffaa1be14bb7d5921529e4
jacqueline orzuna cortes <jacquelineorzuna95@gmail.com>
2019-03-21 13:48:15 -0600

    4

--- js/acciones.js
+++ js/acciones.js
@@ -4,10 +4,9 @@
 	document.addEventListener ("deviceready",function(){
 		$('#disp table td').eq(3).text(device.model);
 		$('#disp table td').eq(5).text(device.cordova);
-		$('#disp table id').eq(7).text(device.olanform);
-		$('#disp table id').eq(9).text(device.version);
-		$('#disp table id').eq(11).text(device.uuid);
+		$('#disp table td').eq(7).text(device.olanform);
+		$('#disp table td').eq(9).text(device.version);
+		$('#disp table td').eq(11).text(device.uuid);
 	},false);//ready device
 	
  });//document
- 
